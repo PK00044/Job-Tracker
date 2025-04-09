96f1ea356ba17336b2e9c934529e604c373dbc72
Pratham Kubsad <prathamkubsad@gmail.com>
2025-04-09 17:56:28 +0530

    Update: modified changes

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -15,11 +15,12 @@ export const addJob = async (req, res) => {
 export const getJobs = async (req, res) => {
   try {
     const jobs = await Job.find();
-    res.status(200).json(jobs);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(200).json(jobs); 
+    
+    res.status(500).json({ message: error.message });
   }
 };
+
 
 // Update job status
 export const updateJob = async (req, res) => {
